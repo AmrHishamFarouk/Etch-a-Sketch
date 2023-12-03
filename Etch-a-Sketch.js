cbd70463371d314db6fbb66d5379ae859e92be9e
AmrHishamFarouk <hishamamr121@gmail.com>
2023-12-03 20:18:06 +0200

    Add files via upload

---
+++ Etch-a-Sketch.js
@@ -0,0 +1,12 @@
+console.log('zeko');
+
+const main = document.createElement('div');
+document.body.appendChild(main);
+main.classList.add('main');
+main.setAttribute('style','width:45vw;height: 45vw;margin: 0 auto;background-color: aqua;');
+
+const row = document.createElement('div');
+row.setAttribute('style','margin:0;padding:0;display:flex;width:40px;height:40px;background-color:green;');
+
+main.appendChild(row)
+main.appendChild(row)
